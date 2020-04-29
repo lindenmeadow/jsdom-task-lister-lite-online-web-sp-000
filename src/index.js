d3f604ee697156d2d43f0b29354cdaab93a4d45c
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-29 17:18:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskDescription = document.getElementById('new-task-description');
+
+ function addTasks (event) {
+   const task = document.createElement('li');
+
+   task.textContent = taskDescription.value + " ";
+
+
+   tasks.appendChild(task);
+
+   taskDescription.value = " ";
+   event.preventDefault();
+ }
+
+
+form.addEventListener("submit", addTasks, false);
 });
